665aa579ce58c1302acd09442f0cdbaa6d188a25
karan <kstkaranthakur03@gmail.com>
2022-04-11 13:25:42 +0530

    this is the put req

--- src/test/java/Assignment2put.java
+++ src/test/java/Assignment2put.java
@@ -5,9 +5,9 @@ import org.json.JSONObject;
 import org.testng.Assert;
 
 import static io.restassured.RestAssured.given;
-public class Assignment2 {
+public class Assignment2put {
     public static void main(String[] args) {
-         //
+        //
         RestAssured.baseURI="https://gorest.co.in";
         RestAssured.basePath="/public/v2/users/3690";//for put we need to pass id also
         JSONObject obj=new JSONObject();
@@ -17,12 +17,12 @@ public class Assignment2 {
         obj.put("status","active");
         //use the headers after the given methods
         Response response=given().header("Content-Type","application/json")
-                 .header("Authorization","Bearer "+ "52f3e15df951a85c92feb6f43c970e00be54cb7ff4a248478123e33bf2705fc8")
+                .header("Authorization","Bearer "+ "52f3e15df951a85c92feb6f43c970e00be54cb7ff4a248478123e33bf2705fc8")
                 .contentType("application/json")
                 .relaxedHTTPSValidation().body(obj.toString())
                 .put();
 //        String ans= response.prettyPrint().toString();
- response.prettyPrint();
+        response.prettyPrint();
         //Assert.assertTrue(ans.contains("emma.wong@reqres.in"));
     }
 }
