e39d8b6337f24f9d2f9c8b0b48720e04dfc5562d
karan <kstkaranthakur03@gmail.com>
2022-04-11 13:30:54 +0530

    this is the delete  req

---
+++ src/test/java/Assignmentdelete.java
@@ -0,0 +1,28 @@
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import org.json.JSONObject;
+import org.testng.Assert;
+
+import static io.restassured.RestAssured.given;
+public class Assignmentdelete {
+    public static void main(String[] args) {
+        //
+        RestAssured.baseURI="https://gorest.co.in";
+        RestAssured.basePath="/public/v2/users/3690";//for put we need to pass id also
+        JSONObject obj=new JSONObject();
+        obj.put("name","Karan Singh Thakur");
+        obj.put("email","karan1122@12gmail.com");
+        obj.put("gender","male");
+        obj.put("status","active");
+        //use the headers after the given methods
+        int status=given().header("Content-Type","application/json")
+                .header("Authorization","Bearer "+ "52f3e15df951a85c92feb6f43c970e00be54cb7ff4a248478123e33bf2705fc8")
+                .contentType("application/json")
+                .relaxedHTTPSValidation()
+                .delete().getStatusCode();
+//        String ans= response.prettyPrint().toString();
+        System.out.println(status);
+        //Assert.assertTrue(ans.contains("emma.wong@reqres.in"));
+    }
+}
